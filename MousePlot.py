50f8b22dac3665d957e51e25947980b433d4ce97
brandaochico <brandao.chico24@gmail.com>
2025-06-12 12:40:25 -0300

    mapping util function

--- MousePlot.py
+++ MousePlot.py
@@ -2,6 +2,7 @@ import pygame
 from pygame.locals import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
+from Utils import *
 
 pygame.init()
 
@@ -9,20 +10,20 @@ screen_width = 1000
 screen_height = 800
 
 screen = pygame.display.set_mode((screen_width, screen_height), DOUBLEBUF | OPENGL)
-pygame.display.set_caption('Graphs in PyOpenGL')
+pygame.display.set_caption('Plotting Points w/ Mouse')
 
 
 def init_ortho():
     glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
-    gluOrtho2D(0, 1000, 800, 0)
+    gluOrtho2D(0, 640, 0, 480)
 
 
 def plot_point():
     glBegin(GL_POINTS)
 
     for p in points:
-        glVertex2i(p[0], p[1])
+        glVertex2f(p[0], p[1])
 
     glEnd()
 
@@ -33,11 +34,14 @@ init_ortho()
 glPointSize(5)
 
 while not done:
+    p = None
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
-        if event.type == MOUSEBUTTONDOWN:
-            points.append(pygame.mouse.get_pos())
+        elif event.type == MOUSEBUTTONDOWN:
+            p = pygame.mouse.get_pos()
+            points.append((map_value(0, screen_width, 0, 640, p[0]),
+                           map_value(0, screen_height, 480, 0, p[1])))
 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     glMatrixMode(GL_MODELVIEW)
